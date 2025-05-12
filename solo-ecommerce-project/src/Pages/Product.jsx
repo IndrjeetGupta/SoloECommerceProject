9616a82ace1d20fd3465af20b01e440cc9f49622
IndrjeetGupta <indrajeet@modalx.ai>
2025-05-12 17:33:23 +0530

    product

--- solo-ecommerce-project/src/Pages/Product.jsx
+++ solo-ecommerce-project/src/Pages/Product.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../Css/Product.css'
 
 function Product() {
+
+  const [product , setProduct] = useState([])
+  const [loading , setLoading] = useState(false) 
+  const [pages, setPages] = useState(2)
+
+
+let url = `http://localhost:3000/products?_page=${pages}&_per_page=8`;
+
+ 
+  const fetchData = async() =>{
+    setLoading(true)
+    try{
+      const response = await fetch(url)
+      const data = await response.json()
+      setLoading(false)
+      setProduct(data.data)
+
+    }
+    catch(err){
+      console.log(err)
+      setLoading(true)
+    }
+
+  }
+  console.log(product)
+  console.log(loading)
+
+  useEffect(() =>{
+    fetchData()
+
+  },[pages])
+
+  const pre = () =>{
+    setPages(pages -1)
+  }
+
+    const next= () =>{
+    setPages(pages +1)
+  }
+  if(loading){
+    return <h1>Loading</h1>
+  }
+
   return (
-    <div>
+    <div id='main'>
+      {
+        product.map((e,index) =>(
+          <div className='card' key={e.id}>
+            {/* <p>{e.category}</p> */}
+            <img src={e.image} alt="" />
+            {/* <button style={{textAlign: 'center'}}>Add To Card</button> */}
+            
+
+          </div>
+        ))
+      }
+      <div>
+        <button onClick={pre}>Pre</button>
+        <button onClick={next}>Next</button>
+        
+      </div>
       
     </div>
   )
